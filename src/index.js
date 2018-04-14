518c6abf1948d351e7c47ff73f7a988eabe17648
haigbedros <haigbedros@gmail.com>
2018-04-14 13:22:12 -0700

    added inital array for the cards and giving them id to be used during the game

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+// const App = () => {
+//     return (
+//     <div>
+//     <Header />
+//     <Footer />
+//     </div>
+//     );
+// }
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
